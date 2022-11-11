8e86c6d71a0bd362d97c0eae0134437a7013e43a
pahuja13 <pahuja31@gmail.com>
2022-11-12 00:04:26 +0200

    updated trip lambda

--- Capstone Project - Taxi Aggregator/Lambdas/TripLambda.py
+++ Capstone Project - Taxi Aggregator/Lambdas/TripLambda.py
@@ -15,55 +15,76 @@ allTrips = database["TripDetails"]
 
 
 def lambda_handler(event, context):
-    # event to start trip received
-    if 'tripstatus' in event and event['tripstatus'] == 'start':
-        # update trip data as inprogress
-        # response trip started
-        return {
-            'status': 200,
-            'description': 'trip started for user ' + event['useremail'] + "by taxi " + event['taxiemail']
-        }
-    # event to end trip received
-    if 'tripstatus' in event and event['tripstatus'] == 'end':
-        # generated random number in range of 1-10 considered as trip duration
-        trip_duration = random.randint(1, 10)
-        # find taxi requesting to end trip
+    requesting_taxi = dict()
+    if 'tripstatus' in event:
         for x in allTaxis.find():
-            if event['taxiemail'] == x['email']:
+            if event['taxiemail'] == x['email'] and event['useremail'] == x['email']:
                 requesting_taxi = x
                 break
-        # update status of taxi as Available again
-        taxi_id = requesting_taxi["_id"]
-        query_update_taxi_status = {"_id": taxi_id}
-        taxi_update = {
-            "$set":
+        if event['tripstatus'] == 'start':
+            trip_record_start_trip_query = allTrips.find_one(
                 {
-                    "tripStatus": "Available"
+                    "taxiemail": requesting_taxi['email'],
+                    "useremail": event['useremail'],
+                    "tripstatus": ""
                 }
-        }
-        allTaxis.update_one(query_update_taxi_status, taxi_update)
-        # get coordinates of taxi at end trip and update in trip table as end coordinate
-        # update random number as trip duration in trip table
-        end_trip_location = requesting_taxi['location']
-        trip_record = allTrips.find_one(
-            {
-                "taxiemail": requesting_taxi['email'],
-                "useremail": event['useremail'],
-                "tripstatus": "inprogress"
-            }
-        )
-        query = {"_id": trip_record["_id"]}
-        # update trip data as ended only where tripstatus is inprogress
-        trip_update = {
-            "$set":
-                {
-                    "endpoint": end_trip_location,
-                    "duration": trip_duration,
-                    "tripstatus": "ended"
+            )
+            query_start_trip = {"_id": trip_record_start_trip_query["_id"]}
+            trip_update_after_start = {
+                "$set":
+                    {
+                        "endpoint": end_trip_location,
+                        "duration": trip_duration,
+                        "tripstatus": "inprogress"
+                    }
                 }
-        }
-        allTrips.update_one(query, trip_update)
+        allTrips.update_one(query_start_trip, trip_update_after_start)
         return {
-            'status': 200,
-            'description': 'trip ended for user ' + event['useremail'] + "by taxi " + event['taxiemail']
-        }
+                'status': 200,
+                'description': 'trip started for user ' + event['useremail'] + "by taxi " + event['taxiemail']
+            }
+    elif event['tripstatus'] == 'end':
+            # generated random number in range of 1-10 considered as trip duration
+            trip_duration = random.randint(1, 10)
+            # update status of taxi as Available again
+            taxi_id = requesting_taxi["_id"]
+            query_update_taxi_status = {"_id": taxi_id}
+            taxi_update = {
+                "$set":
+                    {
+                        "tripStatus": "Available"
+                    }
+            }
+            allTaxis.update_one(query_update_taxi_status, taxi_update)
+            # get coordinates of taxi at end trip and update in trip table as end coordinate
+            # update random number as trip duration in trip table
+            end_trip_location = requesting_taxi['location']
+            trip_record = allTrips.find_one(
+                {
+                    "taxiemail": requesting_taxi['email'],
+                    "useremail": event['useremail'],
+                    "tripstatus": "inprogress"
+                }
+            )
+            query = {"_id": trip_record["_id"]}
+            # update trip data as ended only where tripstatus is inprogress
+            trip_update = {
+                "$set":
+                    {
+                        "endpoint": end_trip_location,
+                        "duration": trip_duration,
+                        "tripstatus": "ended"
+                    }
+            }
+            allTrips.update_one(query, trip_update)
+            return {
+                'status': 200,
+                'description': 'trip ended for user ' + event['useremail'] + "by taxi " + event['taxiemail']
+            }
+    else:
+        return {
+                'status': 500,
+                'description': 'NO trip to start or end'
+            }
+            
+  
