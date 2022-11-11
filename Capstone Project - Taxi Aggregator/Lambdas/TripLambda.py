6aa113310099889bf4fd70e43dec6dc694ee316b
pahuja13 <pahuja31@gmail.com>
2022-11-11 09:27:14 +0200

    formated code for trip lambda

--- Capstone Project - Taxi Aggregator/Lambdas/TripLambda.py
+++ Capstone Project - Taxi Aggregator/Lambdas/TripLambda.py
@@ -23,27 +23,27 @@ def lambda_handler(event, context):
             'description': 'trip started'
         }
     # end trip updated  event received
-        # end trip updated  event received
-        if 'tripstatus' in event and event['tripstatus'] == 'end':
-            trip_duration = random.randint(1, 10)
-            for x in allTaxis.find():
-                if event['taxiemail'] == x['email']:
-                    requesting_taxi = x
-                    break
-            # get coordinates of taxi at end trip and update in trip table as end coordinate
-            # update random number as trip duration in trip table
-            end_trip_location = requesting_taxi['location']
-            trip_record = allTrips.find_one({"taxiemail": requesting_taxi['email']})
-            query = {"_id": trip_record["_id"]}
-            trip_update = {
-                "$set":
-                    {
-                        "endpoint": end_trip_location,
-                        "duration": trip_duration
-                    }
-            }
-            allTrips.update_one(query, trip_update)
-            return {
-                'status': 200,
-                'description': 'trip ended'
-            }
+    # end trip updated  event received
+    if 'tripstatus' in event and event['tripstatus'] == 'end':
+        trip_duration = random.randint(1, 10)
+        for x in allTaxis.find():
+            if event['taxiemail'] == x['email']:
+                requesting_taxi = x
+                break
+        # get coordinates of taxi at end trip and update in trip table as end coordinate
+        # update random number as trip duration in trip table
+        end_trip_location = requesting_taxi['location']
+        trip_record = allTrips.find_one({"taxiemail": requesting_taxi['email']})
+        query = {"_id": trip_record["_id"]}
+        trip_update = {
+            "$set":
+                {
+                    "endpoint": end_trip_location,
+                    "duration": trip_duration
+                }
+        }
+        allTrips.update_one(query, trip_update)
+        return {
+            'status': 200,
+            'description': 'trip ended'
+        }
